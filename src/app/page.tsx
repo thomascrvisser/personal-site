1f5b43ec5b944d5bc029861e07c94666fb965638
thomascrvisser <thomascrvisser@gmail.com>
2023-07-12 20:44:58 -0700

    replace name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main className="grid place-items-center h-screen w-screen">
       <p className="font-serif max-w-xs text-justify">
-        test visser is a{" "}
+        thomas visser is a{" "}
         <a
           href="https://www.linkedin.com/in/thomas-visser-79981a182/"
           target="_blank"
