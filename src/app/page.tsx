21d45219e468bd015c9c3f58506131a9e09ace67
Thomas Visser <31866160+thomascrvisser@users.noreply.github.com>
2025-07-08 14:01:47 -0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,20 +6,40 @@ export default function Home() {
         <a
           href="https://www.linkedin.com/in/thomas-visser-79981a182/"
           target="_blank"
+          rel="noopener noreferrer"
         >
           SOFTWARE ENGINEER
         </a>{" "}
-        at{" "}
-        <a href="https://www.lovevery.com" target="blank">
-          LOVEVERY
-        </a>{" "}
-        living in{" "}
-        <a href="https://www.google.com/search?q=PORTLAND" target="_blank">
+        based in{" "}
+        <a
+          href="https://www.google.com/search?q=PORTLAND"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           PORTLAND
         </a>
+        . he has worked at companies like{" "}
+        <a
+          href="https://www.lovevery.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          LOVEVERY
+        </a>{" "}
+        and{" "}
+        <a
+          href="https://academy.guildmortgage.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          ACADEMY MORTGAGE
+        </a>
         . feel free to{" "}
-        <a href="mailto:thomascrvisser@gmail.com" target="_blank">
-          {" "}
+        <a
+          href="mailto:thomascrvisser@gmail.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           REACH OUT
         </a>
         .
